eb66c291b6731e7944b62f7bd5cb1eb033fcc5f6
JackMarx <seriousfools@gmail.com>
2018-10-25 18:44:26 -0500

    list tests

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe List, type: :model do
+  describe '#complete_all_tasks!' do 
+    it 'should mark all tasks from the list as complete' do 
+      list = List.create(name: "Shopping")
+      Task.create(complete: false, list_id: list.id, name: "Milk")
+      Task.create(complete: false, list_id: list.id, name: "Eggs")
+
+      list.complete_all_tasks!
+
+      list.tasks.each do |task|
+        expect(task.complete).to eq(true)
+      end
+    end
+  end
+end
